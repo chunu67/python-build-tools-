09f5115976f274beb6301f469d10c1d61128ce01
Rob Nelson <nexisentertainment@gmail.com>
2014-09-27 04:57:12 -0700

    Add cmake wrapper.

--- buildtools/wrapper/__init__.py
+++ buildtools/wrapper/__init__.py
@@ -1,2 +1,3 @@
 from .ant import Ant
 from .fpm import FPM
+from .cmake import CMake
---
+++ buildtools/wrapper/cmake.py
@@ -0,0 +1,24 @@
+import os
+
+from buildtools.bt_logging import log
+from buildtools.os_utils import cmd
+
+class CMake(object):
+    def __init__(self):
+        self.flags = []
+        
+    def setFlag(self, key, val):
+        log.info('CMake: {} = {}'.format(key, val))
+        self.flags[key] = val
+        
+    def run(cfg, env=os.environ, dir='.'):
+        CMAKE = cfg.get('bin.cmake', 'cmake')
+        flags = []
+        
+        for key, value in self.flags.items():
+            flags += ['-D{0}={1}'.format(key, value)]
+            
+        with log.info('Running CMake:'):
+            for key, value in BUILD_ENV.items():
+                log.info('+{0}="{1}"'.format(key, value))
+            cmd([CMAKE] + flags + [dir], env=env, critical=True, echo=True)
