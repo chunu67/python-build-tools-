2023064ad729103d6d1bc2cb94d68b1e207025dc
Rob Nelson <nexisentertainment@gmail.com>
2015-12-21 11:14:11 -0800

    Add setuptools script.

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="buildtools",
+    version="0.0.1",
+    description=("Simple library implementing common processes and logging for buildsystems"),
+    author="Rob Nelson",
+    author_email="nexisentertainment@gmail.com",
+    packages=find_packages(),
+    install_requires=[
+        "psutil",
+        "lxml",
+        "twisted",
+        "pyyaml",
+        "jinja2",
+        "toml",
+        "requests>=2.0",
+    ],
+    tests_require=[
+        "pytest",
+        "mock==1.0.1",
+    ],
+    extras_require={
+        "development": [
+            "pylint",
+        ],
+    },
+    license="MIT License",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 2.7",
+    ],
+)
