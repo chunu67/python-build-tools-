f769fa8da4aa03130ea8700b7640e5da4d742bad
Rob Nelson <nexisentertainment@gmail.com>
2017-05-13 02:59:45 -0700

    Backwards compat, maybe

--- buildtools/http.py
+++ buildtools/http.py
@@ -22,6 +22,8 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 
 '''
+from __future__ import print_function
+
 import logging
 
 from buildtools.bt_logging import log
