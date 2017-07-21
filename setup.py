a013a2f65b99f8eaa0cb6f92925b56019d8c3acc
Rob Nelson <nexisentertainment@gmail.com>
2017-07-21 15:56:41 -0700

    Version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="buildtools",
-    version="0.0.2",
+    version="0.2.0",
     description=("Simple library implementing common processes and logging for buildsystems"),
     author="Rob Nelson",
     author_email="nexisentertainment@gmail.com",
