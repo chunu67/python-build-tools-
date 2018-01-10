5bd2341a8320355bc2491e095dcf192138a6329f
Rob Nelson <nexisentertainment@gmail.com>
2018-01-09 23:21:36 -0800

    Update copyright shit

--- buildtools/as3.py
+++ buildtools/as3.py
@@ -1,7 +1,7 @@
 '''
 ActionScript3-related shit
 
-Copyright (c) 2015 - 2017 Rob "N3X15" Nelson <nexisentertainment@gmail.com>
+Copyright (c) 2015 - 2018 Rob "N3X15" Nelson <nexisentertainment@gmail.com>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -20,6 +20,7 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
+
 '''
 
 import os
