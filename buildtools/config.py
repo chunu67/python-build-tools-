6365b1a63c0f5aa6f7529b56e1d38603528f895a
Rob Nelson <nexisentertainment@gmail.com>
2019-03-10 19:55:15 -0700

    Copyright bump.

--- buildtools/config.py
+++ buildtools/config.py
@@ -1,7 +1,7 @@
 '''
 Salty Configuration, meaning that a bunch of this code is stolen from Salt :V
 
-Copyright (c) 2015 - 2018 Rob "N3X15" Nelson <nexisentertainment@gmail.com>
+Copyright (c) 2015 - 2019 Rob "N3X15" Nelson <nexisentertainment@gmail.com>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
