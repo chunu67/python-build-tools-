7f1158594f4f9fd2eb90b9d475ae20ddde963e2d
Rob Nelson <nexisentertainment@gmail.com>
2020-01-24 19:02:27 -0800

    Copyright update, vnm support.

--- buildtools/wrapper/fpm.py
+++ buildtools/wrapper/fpm.py
@@ -1,7 +1,7 @@
 '''
 FPM Wrapper
 
-Copyright (c) 2015 - 2019 Rob "N3X15" Nelson <nexisentertainment@gmail.com>
+Copyright (c) 2015 - 2020 Rob "N3X15" Nelson <nexisentertainment@gmail.com>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
